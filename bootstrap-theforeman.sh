79b195f7520a9f61a3c918d169dc5d41da2e61b9
Chris Scheib <chris@schweeb.org>
2014-12-15 19:28:34 -0500

    autorun foreman-install during bootstrap

--- bootstrap-theforeman.sh
+++ bootstrap-theforeman.sh
@@ -1,6 +1,10 @@
-#wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
-#dpkg -i puppetlabs-release-trusty.deb
+# TheForeman apparently doesn't like puppet 3.7, use version that comes in trusty (3.4.x)
+##wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
+##dpkg -i puppetlabs-release-trusty.deb
+
 echo "deb http://deb.theforeman.org/ trusty 1.7" > /etc/apt/sources.list.d/foreman.list
 echo "deb http://deb.theforeman.org/ plugins 1.7" >> /etc/apt/sources.list.d/foreman.list
 wget -q http://deb.theforeman.org/pubkey.gpg -O- | apt-key add -
 apt-get update && apt-get -y install foreman-installer
+
+foreman-installer
